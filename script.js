0f33a521d35114bab1f54a86ab8d3a17a9a9e515
SlimTracy <90283597+SlimTracy@users.noreply.github.com>
2025-01-19 16:14:26 -0600

    fixed image on click

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
     // Open Modal on Image Click
     document.querySelectorAll(".clickable-photo").forEach(photo => {
         photo.addEventListener("click", () => {
-            modal.style.display = "block";
-            modalImage.src = photo.src;
+            modal.style.display = "block"; // Show the modal
+            modalImage.src = photo.src; // Set the clicked image source in modal
         });
     });
 
     // Close Modal on Close Button Click
     closeModal.addEventListener("click", () => {
-        modal.style.display = "none";
+        modal.style.display = "none"; // Hide the modal
     });
 
     // Close Modal on Outside Click
     window.addEventListener("click", event => {
         if (event.target === modal) {
-            modal.style.display = "none";
+            modal.style.display = "none"; // Hide the modal if clicked outside
         }
     });
 });
